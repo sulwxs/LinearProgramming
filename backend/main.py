ebe7a05147a7bf389e89c394761db70d1ce7c837
cypzwbw <641625694@qq.com>
2025-02-27 17:15:02 +0800

    更新

--- backend/main.py
+++ backend/main.py
@@ -13,6 +13,8 @@ app = Flask(__name__)
 CORS(app)
 UPLOAD_FOLDER = 'uploads'
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+if not os.path.exists(app.config['UPLOAD_FOLDER']):
+    os.mkdir(app.config['UPLOAD_FOLDER'])
 ALLOWED_EXTENSIONS = {'txt', 'csv'}
 matrixs={}
 @app.route("/")
